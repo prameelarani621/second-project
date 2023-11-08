614b080f2108229b7c84e4c18dade2d38781a6de
prameelarani621 <raniprameela221@gmail.com>
2023-11-08 13:18:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def ammu(request):
+    return HttpResponse('ammu what r u doing')
+
+def mani(request):
+    return HttpResponse('mani how r u when do u came')
